0fd0a02614c839cda5d6e2afbf7eb4e79b0f08d7
Srim <deamoon0801@naver.com>
2021-02-03 17:35:05 +0900

    [ADD] style config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   entry: ["./src/index.tsx"],
   output: { filename: "bundle.js", path: __dirname + "dist" },
-  module: { rules: [{ test: /\.(js|jsx|ts|tsx)$/, exclude: /node_modules/, use: ["babel-loader"] }] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
 };
